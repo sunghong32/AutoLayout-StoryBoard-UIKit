e077e891d3319bd288c0a5b6b4929e4a11d57dc4
민성홍 <sunghong32@naver.com>
2022-03-29 21:04:50 +0900

    make TodoList app, add navigationView, TableView, setting tableViewCell

---
+++ TodoList/TodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by 민성홍 on 2022/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
