77f229e1214adc73500f65d39cdc4a5a781c79be
민성홍 <sunghong32@naver.com>
2022-03-29 20:49:18 +0900

    add function operation Button

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -55,23 +55,61 @@ class ViewController: UIViewController {
     }
 
     @IBAction func tapDivideButton(_ sender: UIButton) {
-
+        self.operation(.divide)
     }
 
     @IBAction func tapMultiplyButton(_ sender: UIButton) {
-
+        self.operation(.multiply)
     }
 
     @IBAction func tapSubtractButton(_ sender: UIButton) {
-
+        self.operation(.subtract)
     }
 
     @IBAction func tapAddButton(_ sender: UIButton) {
-
+        self.operation(.add)
     }
 
     @IBAction func tapEqulButton(_ sender: UIButton) {
+        self.operation(self.currentOperation)
+    }
 
+    func operation(_ operation: Operation) {
+        if self.currentOperation != .unknown {
+            if self.displayNumber.isEmpty == false {
+                self.secondOperand = self.displayNumber
+                self.displayNumber = ""
+
+                guard let firstOperand = Double(self.firstOperand) else { return }
+                guard let secondOperand = Double(self.secondOperand) else { return }
+
+                switch self.currentOperation {
+                    case .add:
+                        self.result = "\(firstOperand + secondOperand)"
+                    case .subtract:
+                        self.result = "\(firstOperand - secondOperand)"
+                    case .divide:
+                        self.result = "\(firstOperand / secondOperand)"
+                    case .multiply:
+                        self.result = "\(firstOperand * secondOperand)"
+                    default :
+                        break
+                }
+
+                if let result = Double(self.result), result.truncatingRemainder(dividingBy: 1) == 0 {
+                    self.result = "\(Int(result))"
+                }
+
+                self.firstOperand = self.result
+                self.numberOutputLabel.text = self.result
+            }
+
+            self.currentOperation = operation
+        } else {
+            self.firstOperand = self.displayNumber
+            self.currentOperation = operation
+            self.displayNumber = ""
+        }
     }
 }
 
