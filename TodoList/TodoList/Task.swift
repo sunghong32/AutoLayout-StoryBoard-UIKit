5b7ea116b8b9c0e03596aed5168d3369230d1b45
민성홍 <sunghong32@naver.com>
2022-03-29 21:35:45 +0900

    add function adding Task using dequeueReusable Cell

---
+++ TodoList/TodoList/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  TodoList
+//
+//  Created by 민성홍 on 2022/03/29.
+//
+
+import Foundation
+
+struct Task {
+    var title: String
+    var done: Bool
+}
--- TodoList/TodoList/ViewController.swift
+++ TodoList/TodoList/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+
+    var tasks = [Task]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        self.tableView.dataSource = self
     }
 
+    @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
 
+    }
+
+    @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "할일 등록", message: nil, preferredStyle: .alert)
+        let registerButton = UIAlertAction(title: "등록", style: .default, handler: { [weak self] _ in
+            guard let title = alert.textFields?[0].text else { return }
+            let task = Task(title: title, done: false)
+            self?.tasks.append(task)
+            self?.tableView.reloadData()
+        })
+
+        let cancelButton = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+
+        alert.addAction(cancelButton)
+        alert.addAction(registerButton)
+        alert.addTextField(configurationHandler: { textField in
+            textField.placeholder = "할 일을 입력해주세요."
+        })
+
+        self.present(alert, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tasks.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let task = self.tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+
+        return cell
+    }
 }
 
