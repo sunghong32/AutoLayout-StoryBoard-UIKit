b39e0002a523cbcfeb253bc58ac788a96047cdbc
민성홍 <sunghong32@naver.com>
2022-04-04 20:41:31 +0900

    add diary enroll view using TextField, TextView

---
+++ Diary/Diary/WriteDiaryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WriteDiaryViewController.swift
+//  Diary
+//
+//  Created by 민성홍 on 2022/04/04.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+}
