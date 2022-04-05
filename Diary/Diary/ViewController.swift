5b6831f00800aa3fef6a920a15b5d8b572f1140a
민성홍 <sunghong32@naver.com>
2022-04-05 20:03:54 +0900

    save diary data using userDefaults, add border CollectionView

--- Diary/Diary/ViewController.swift
+++ Diary/Diary/ViewController.swift
@@ -10,12 +10,17 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var collectionView: UICollectionView!
 
-    private var diaryList = [Diary]()
+    private var diaryList = [Diary]() {
+        didSet {
+            self.saveDiaryList()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.configureCollectionView()
+        self.loadDiaryList()
     }
 
     private func configureCollectionView() {
@@ -31,6 +36,35 @@ class ViewController: UIViewController {
         }
     }
 
+    private func saveDiaryList() {
+        let date = self.diaryList.map {
+            [
+                "title": $0.title,
+                "contents": $0.contents,
+                "date": $0.date,
+                "isStar": $0.isStar
+            ]
+        }
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(date, forKey: "diaryList")
+    }
+
+    private func loadDiaryList() {
+        let userDefaults = UserDefaults.standard
+        guard let data = userDefaults.object(forKey: "diaryList") as? [[String: Any]] else { return }
+        self.diaryList = data.compactMap {
+            guard let title = $0["title"] as? String else { return nil }
+            guard let contents = $0["contents"] as? String else { return nil }
+            guard let date = $0["date"] as? Date else { return nil }
+            guard let isStar = $0["isStar"] as? Bool else { return nil }
+
+            return Diary(title: title, contents: contents, date: date, isStar: isStar)
+        }
+        self.diaryList = self.diaryList.sorted(by: {
+            $0.date.compare($1.date) == .orderedDescending
+        })
+    }
+
     private func dateToString(date: Date) -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = "yy년 MM월 dd일 (E)"
@@ -42,6 +76,9 @@ class ViewController: UIViewController {
 extension ViewController: WriteDiaryViewDelegate {
     func didSelectRegister(diary: Diary) {
         self.diaryList.append(diary)
+        self.diaryList = self.diaryList.sorted(by: {
+            $0.date.compare($1.date) == .orderedDescending
+        })
         self.collectionView.reloadData()
     }
 }
